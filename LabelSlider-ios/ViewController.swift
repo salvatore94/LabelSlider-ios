b9756d20b9878497a9f252ab85e1b02db0e8c787
Salvatore Polito <thedarkhacker94@gmail.com>
2017-06-29 12:03:49 +0200

    Code cleanups and better implementation of touchedMoved.

--- LabelSlider-ios/ViewController.swift
+++ LabelSlider-ios/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let lslider = LabelSlider(frame: CGRect(x: 0, y: 0, width: 360, height: 20))
+    let lslider = LabelSlider(frame: CGRect(x: 0, y: 0, width: 360, height: 40))
     let valueLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
     var touchInsideSliderView = false
     
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         lslider.names = ["1/500", "1/250", "1/125", "1/100", "1/50", "1/25", "1/20", "1/15", "1/10", "1/5","1/3"]
         lslider.value = 0
         lslider.ticksDistance = lslider.bounds.width / CGFloat(lslider.tickCount)
-        lslider.animationDuration = 0.1
+        lslider.animationDuration = 0.15
         
         /*
          Per gestire gli eventi di touchedBegan e touchedMove da questa view (ViewController) è necessario disattivare la userInteractionForSubviews per lslider ed attivare la isUserInteractionEnabled per view. A questo punto basta
@@ -31,13 +31,20 @@ class ViewController: UIViewController {
         view.isUserInteractionEnabled = true
         
         
+        //IMPORTANTE bisogna dare un colore di background allo slider altrimenti verranno riconosciute solo le interazioni che iniziano
+        //esattamante sulle sue subview (cioè quelle che iniziano toccando ESATTAMENTE su una label!)
+        lslider.backgroundColor = .black
+        
+        
         //too many names for portrait orientation
+        //Ricordarsi di aumentare la dimensione in height di lslider altrimenti quando applico la rotazione le varie subviews hanno una
+        //larghezza superiore rispetto ad lslider, di conseguenza si hanno problemi a gestire gli eventi uitouch
         lslider.rotationAngle = CGFloat.pi / 2
         
         // Graphic tuning
         lslider.emphasisLayout = 4
-        lslider.upFontSize = 15
-        lslider.downFontSize = 13
+        lslider.upFontSize = 14
+        lslider.downFontSize = 11
         lslider.downFontColor = UIColor.gray
         lslider.upFontColor = UIColor.yellow
         
@@ -60,10 +67,12 @@ class ViewController: UIViewController {
         valueLabel.text = lslider.names[Int(lslider.value)]
         view.addSubview(valueLabel)
         
+        
+        lslider.autoresizesSubviews = true
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (lslider.frame.contains(touches.first!.location(in: view)) || touchInsideSliderView == true) {
+        if (lslider.frame.contains(touches.first!.location(in: view))) {
             touchInsideSliderView = true
             lslider.touchesBegan(touches, with: event)
             valueLabel.text = lslider.names[Int(lslider.value)]
@@ -72,14 +81,14 @@ class ViewController: UIViewController {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if(touchInsideSliderView == true) {
+        if(touchInsideSliderView == true || lslider.frame.contains(touches.first!.location(in: view))) {
             lslider.touchesMoved(touches, with: event)
             valueLabel.text = lslider.names[Int(lslider.value)]
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if(touchInsideSliderView == true) {
+        if(touchInsideSliderView == true || lslider.frame.contains(touches.first!.location(in: view))) {
             lslider.touchesEnded(touches, with: event)
             valueLabel.text = lslider.names[Int(lslider.value)]
             touchInsideSliderView = false
@@ -87,7 +96,7 @@ class ViewController: UIViewController {
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (touchInsideSliderView == true) {
+        if (touchInsideSliderView == true || lslider.frame.contains(touches.first!.location(in: view))) {
             lslider.touchesCancelled(touches, with: event)
             valueLabel.text = lslider.names[Int(lslider.value)]
             touchInsideSliderView = false
